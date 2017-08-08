eb3542e49d3f645a77fc6c6b9c63c69a410e6617
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-08-08 17:50:50 -0400

    Ok

---
+++ barCode/Models/RespuestaLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace barCode.Models
+{
+    public class RespuestaLogin
+    {
+        public string mensaje;
+        public bool estado;
+        public string observacion;
+    }
+
+    public class RespuestaCuentas
+    {
+        public string mensaje;
+        public List<Cuentas> cuentas;
+    }
+
+}
