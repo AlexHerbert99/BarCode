de9b6149fcc702a3e25a9a692eac71da1402ea6a
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-08-08 23:37:05 -0400

    Confirmo

---
+++ barCode/Controllers/PasarelaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace barCode.Controllers
+{
+    public class PasarelaController : Controller
+    {
+        // GET: Pasarela
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
