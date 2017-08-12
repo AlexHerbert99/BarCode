1729877e93928231cc8bfe39629dcdee97b96627
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-08-12 16:48:53 -0400

    Ok

--- barCode/Controllers/BancoFalsoController.cs
+++ barCode/Controllers/BancoFalsoController.cs
@@ -32,8 +32,21 @@ namespace barCode.Controllers
             return Json(r.estado, JsonRequestBehavior.AllowGet);
         }
 
-    }
+        public JsonResult TraerCuentasBcoFalso(string user, string pass)
+        {
+            WebClient wc = new WebClient();
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("apikey", "3333333");
+            nvc.Add("user", user);
+            nvc.Add("pass", pass);
 
+            byte[] result = wc.UploadValues(urlbase + "TraerCuentas", nvc);
+            string JsonResult = Encoding.UTF8.GetString(result);
+            RespuestaCuentas rc = JsonConvert.DeserializeObject<RespuestaCuentas>(JsonResult);
+            return Json(rc, JsonRequestBehavior.AllowGet);
+
+        }
+    }
 }
 
         
