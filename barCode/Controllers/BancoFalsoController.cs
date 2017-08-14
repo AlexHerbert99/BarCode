a168ca5899bc3c5af8aecc668c704f2944dcafad
AlexHerbert99 <alex.munozs@correoaiep.cl>
2017-08-14 18:22:45 -0300

    Pagos con Banco Falso

--- barCode/Controllers/BancoFalsoController.cs
+++ barCode/Controllers/BancoFalsoController.cs
@@ -14,31 +14,30 @@ namespace barCode.Controllers
 {
     public class BancoFalsoController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-
         barCodePruebaEntities db = new barCodePruebaEntities();
         string urlbase = "http://www.devkairos.com/BancoFalsoportal/serviciosbancofalso/";
         string rut = "16243551-5";
         string clave = "123456";
         string apik = "3333333";
 
-        // GET: BancoFalso
-        public JsonResult LoginBco()
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]       
+        public JsonResult LoginBco(string user, string pass)
         {
             WebClient wc = new WebClient();
             NameValueCollection nv = new NameValueCollection();
             nv.Add("apikey", apik);
-            nv.Add("user", rut);
-            nv.Add("pass", clave);
+            nv.Add("user", user);
+            nv.Add("pass", pass);
 
             byte[] result = wc.UploadValues(urlbase + "Login", nv);
             string JsonLogin = Encoding.UTF8.GetString(result);
             RespuestaLogin r = JsonConvert.DeserializeObject<RespuestaLogin>(JsonLogin);
-            return Json(r.estado, JsonRequestBehavior.AllowGet);
+            return Json(r.estado, JsonRequestBehavior.AllowGet);            
         }
 
         public JsonResult TraerCuentas()
@@ -55,17 +54,17 @@ namespace barCode.Controllers
             return Json(rc, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult Pagar()
+        public JsonResult Pagar(string monto, string descipcion, string idPedido, string idCuenta)
         {
             WebClient wc = new WebClient();
             NameValueCollection nvc = new NameValueCollection();
             nvc.Add("apikey", apik);
             nvc.Add("user", rut);
             nvc.Add("pass", clave);
-            nvc.Add("monto", "10000");
-            nvc.Add("descripcion", "Resumen Carrito");
-            nvc.Add("idPedido", "5005");
-            nvc.Add("idCuenta", "7");
+            nvc.Add("monto", monto);
+            nvc.Add("descripcion", descipcion);
+            nvc.Add("idPedido", idPedido);
+            nvc.Add("idCuenta", idCuenta);
 
             byte[] result = wc.UploadValues(urlbase + "Pagar", nvc);
             string JsonResult = Encoding.UTF8.GetString(result);
@@ -75,6 +74,5 @@ namespace barCode.Controllers
     }
 }
 
-        
 
-       
+
